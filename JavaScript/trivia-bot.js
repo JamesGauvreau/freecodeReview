b97d94db5835d8c26a0eba960103de92e1dc43c7
cmSalticidae <james.longfrenchname@gmail.com>
2025-05-06 00:19:48 -0700

    worked on trivia bot

---
+++ JavaScript/trivia-bot.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Hattie";
+let botLocation = "La Islo de la Luno";
+let favoriteLanguage = "JavaScript";
+
+let phrase2 = "My name is " + botName + " and I live on " + botLocation + ".";
+let phrase3 = "My favorite programming language is " + favoriteLanguage + ".";
+
+let codingFact = "This isn't a " + favoriteLanguage + " fact at all.";
+
+// let openingPhrase = `My name is ${botName} and I live on ${botLocation}.`;
+
+console.log(phrase2);
+console.log(phrase3);
+console.log(codingFact);
+
+codingFact = "This " + favoriteLanguage + " fact has been redeclared.";
+
+console.log(codingFact);
+
+codingFact = "Once more this is a redeclared " + favoriteLanguage + " fact.";
+
+console.log(codingFact);
+
+let phrase4 = "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".";
+
+console.log(phrase4)
