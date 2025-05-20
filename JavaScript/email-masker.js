d9b8e4d2a1f78c90ecfb1cb2e6e33ad991786aa7
cmSalticidae <james.longfrenchname@gmail.com>
2025-05-19 21:29:50 -0700

    fixed version

--- JavaScript/email-masker.js
+++ JavaScript/email-masker.js
@@ -1,17 +1,26 @@
 function maskEmail(email) {
-    const endValue = email.indexOf(`@`);
+    const endValue = email.indexOf(`@`) - 1;
     const replaceChar = "*";
     const slice1 = email.slice(0,1);
     const sliceMid = email.slice(1,endValue);
-    const startValue = sliceMid.length - 1;
+    const startValue = sliceMid.length;
     const replacement = replaceChar.repeat(startValue);
     const slice2 = email.slice(endValue);
     // return replacement + " " + endValue;
     return slice1 + replacement + slice2;
 };
 
-const testEmail = "appledash@electron.com";
-const testEmailDuo = "latchlonde@bark.com";
+// const email = "apple.pie@example.com";
+const email = "freecodecamp@example.com";
+// // const testEmailDuo = "latchlonde@bark.com";
 
-console.log(maskEmail(testEmail));
-console.log(maskEmail(testEmailDuo));
+console.log(maskEmail(email));
+// // console.log(maskEmail(testEmailDuo));
+
+// apple.pie@example.com
+// a*******e@example.com
+// a******e@example.com
+
+// freecodecamp@example.com
+// f**********p@example.com
+// f*********p@example.com
