a531c69654a12afa52b033cf26f08561e1cad013
cmSalticidae <james.longfrenchname@gmail.com>
2025-05-19 21:21:28 -0700

    first viable version

--- JavaScript/email-masker.js
+++ JavaScript/email-masker.js
@@ -1,7 +1,17 @@
 function maskEmail(email) {
-    return email;
+    const endValue = email.indexOf(`@`);
+    const replaceChar = "*";
+    const slice1 = email.slice(0,1);
+    const sliceMid = email.slice(1,endValue);
+    const startValue = sliceMid.length - 1;
+    const replacement = replaceChar.repeat(startValue);
+    const slice2 = email.slice(endValue);
+    // return replacement + " " + endValue;
+    return slice1 + replacement + slice2;
 };
 
 const testEmail = "appledash@electron.com";
+const testEmailDuo = "latchlonde@bark.com";
 
 console.log(maskEmail(testEmail));
+console.log(maskEmail(testEmailDuo));
