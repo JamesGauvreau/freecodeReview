eaa8f77b40d92d9b95dc14326ccee4693bdf0c95
cmSalticidae <james.longfrenchname@gmail.com>
2025-05-21 00:09:52 -0700

    completed lab

---
+++ JavaScript/leap-year-calculator.js
@@ -0,0 +1,22 @@
+const year = 1990;
+
+function isLeapYear(year) {
+    if (year % 4 === 0) {
+        if (year % 100 !== 0) {
+            return `${year} is a leap year.`
+        } else {
+            if (year % 400 === 0) {
+                return `${year} is a leap year.`
+            } else {
+                return `${year} is not a leap year.`
+            }
+        };
+    } else {
+        return `${year} is not a leap year.`
+    }
+}
+
+
+const result = isLeapYear(year);
+
+console.log(result);
