d9c56f32ce93019942fc9d53a448fd29f531b8d3
cmSalticidae <james.longfrenchname@gmail.com>
2025-05-26 22:40:15 -0700

    updated progress

--- JavaScript/notes.js
+++ JavaScript/notes.js
@@ -3,3 +3,6 @@
 // + re-declaring variables; a use for "let"
 
 // don't forget about arrow functions. 
+
+// array.join(', ') is particularly useful.
+// should use arguments in functions more often.
