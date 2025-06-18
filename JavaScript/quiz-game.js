145b6ae16b076a8a5b01d9b365a2f9e5c46227f0
cmSalticidae <james.longfrenchname@gmail.com>
2025-06-17 23:57:48 -0700

    random number generator running

---
+++ JavaScript/quiz-game.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        category: "Dogs",
+        question: "How are dogs?",
+        choices: ["Question the First", "Question the Second", "Question the Third"],
+        answer: "Question the First",
+    },
+    {
+        category: "Dogs",
+        question: "When are dogs?",
+        choices: ["Question the First", "Question the Second", "Question the Third"],
+        answer: "Question the Second",
+    },
+    {
+        category: "Cats",
+        question: "How are cats?",
+        choices: ["Question the First", "Question the Second", "Question the Third"],
+        answer: "Question the Third",
+    },
+    {
+        category: "Cats",
+        question: "When are cats?",
+        choices: ["Question the First", "Question the Second", "Question the Third"],
+        answer: "Question the First",
+    },
+    {
+        category: "Cats",
+        question: "Why are cats?",
+        choices: ["Question the First", "Question the Second", "Question the Third"],
+        answer: "Question the Second",
+    },
+];
+
+function randomNumber(number) {
+    return Math.floor(Math.random() * number);
+};
+
+function getRandomQuestion(questions_array) {
+    // takes an array of questions as a parameter and returns a random question object from 
+    // the array;
+};
+
+function getRandomComputerChoice(choices_array) {
+    // takes array, returns a random answer to the selected question
+};
+
+function getResults(questions_array, computer_choice) {
+    // The function should return The computer's choice is correct! if the answer is correct. Otherwise, it returns The computer's choice is wrong. The correct answer is: <correct-answer>, where <correct-answer> is the value of the correct answer to the chosen question.
+}
+
+console.log(randomNumber(5));
